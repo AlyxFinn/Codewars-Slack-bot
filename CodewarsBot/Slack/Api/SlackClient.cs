221fc65ca282b40fd82726a3f5404f4d118e377d
Alex Finnigan <8987004+AlyxFinn@users.noreply.github.com>
2022-01-06 22:21:36 +0000

    Final bits

---
+++ CodewarsBot/Slack/Api/SlackClient.cs
@@ -0,0 +1,57 @@
+﻿using CodewarsBot.Slack.Models;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Configuration;
+using System.Net;
+
+namespace CodewarsBot.Slack.Api
+{
+    public class SlackClient : ISlackClient
+    {
+        private readonly IRestClient _client;
+
+        public SlackClient()
+        {
+            var webhook = ConfigurationManager.AppSettings["SlackWebHook"];
+            _client = new RestClient(webhook);
+        }
+
+        public bool PostLeaderboardToSlack(string leaderboard)
+        {
+            const string endpoint = "/T02T116NZTM/B02TQNED7U0/8kvEzRyUjtXQSP6No9CeRUAV";
+            RestRequest request = new RestRequest(endpoint, Method.POST);
+
+            request.AddHeader("Content-Type", "application/json");
+
+            var message = new SlackMessage
+            {
+                Text = leaderboard
+            };
+
+            var requestBody = JsonConvert.SerializeObject(message);
+            request.AddParameter("application/json", requestBody, ParameterType.RequestBody);
+
+            try
+            {
+                var response = _client.Execute(request);
+
+                if (response.StatusCode == HttpStatusCode.OK && response.Content == "ok")
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return false;
+        }
+    }
+
+    public interface ISlackClient
+    {
+        bool PostLeaderboardToSlack(string leaderboard);
+    }
+}
---
+++ CodewarsBot/Slack/Models/SlackMessage.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace CodewarsBot.Slack.Models
+{
+    public class SlackMessage
+    {
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+}
