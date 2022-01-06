02d96f3eec3f9ad42961330233d587fdba6c3809
Alex Finnigan <8987004+AlyxFinn@users.noreply.github.com>
2022-01-06 22:19:38 +0000

    Added additional missing files

---
+++ CodewarsBot/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CodewarsBot
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CodewarsBot bot = new CodewarsBot();
+
+            var leaderboard = bot.GenerateClanLeaderboard();
+            bot.SendClanLeaderboard(leaderboard);
+
+        }
+    }
+}
